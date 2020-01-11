b2d27589a57aeaca16dab557c834dab9250f031d
Rupesh <patilrupesh990@gmail.com>
2020-01-11 10:03:51 +0530

    [Rupesh].updated UserService

--- src/main/java/com/bridgelabz/fundooapi/services/UserService.java
+++ src/main/java/com/bridgelabz/fundooapi/services/UserService.java
@@ -1,10 +1,37 @@
-package com.bridgelabz.restapi.services;
+package com.bridgelabz.fundooapi.services;
 
-import com.bridgelabz.restapi.model.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+
+import com.bridgelabz.fundooapi.model.User;
 
 
 public interface UserService{
- 
+
 	public User findUserByEmail(String email);
-	public void saveUser(User user);    
+
+	public User findUserByUserName(String userName);
+
+	public ResponseEntity<String>  registerUser(User user,BindingResult result);   
+
+
+
+
+//	public boolean loginUser(String email, String password);
+//	public boolean userExists(User user);
+//	public User getUserByEmail(String email, User user);
+//	public User getUserById(Integer id, User user);
+//	public void activateUser(Integer id);
+//	public void sendRegistrationVerificationLink(Integer id, String email)
+//			throws FileNotFoundException, ClassNotFoundException, IOException;
+//	public void sendLoginVerificationToken(User user, Token accessToken, HttpServletRequest request)
+//			throws FileNotFoundException, ClassNotFoundException, IOException;
+//	public void sendResetPasswordMail(User user, HttpServletRequest request, Token forgotToken)
+//			throws FileNotFoundException, ClassNotFoundException, IOException;
+//	public void resetPassword(User user);
+
+
+
+
+
 }
