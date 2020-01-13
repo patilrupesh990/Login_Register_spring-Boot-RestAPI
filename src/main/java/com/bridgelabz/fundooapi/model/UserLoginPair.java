b31de3b4ac30d85a5bcf5258e912eafbdebb34db
Rupesh <patilrupesh990@gmail.com>
2020-01-13 19:25:55 +0530

    [Rupesh patil].added UserLoginPair

---
+++ src/main/java/com/bridgelabz/fundooapi/model/UserLoginPair.java
@@ -0,0 +1,39 @@
+package com.bridgelabz.fundooapi.model;
+
+public class UserLoginPair {
+
+	private String email;
+	private String password;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "UserLoginPair [email=" + email + ", password=" + password + "]";
+	}
+
+	public UserLoginPair(String email, String password) {
+		super();
+		this.email = email;
+		this.password = password;
+	}
+
+	public UserLoginPair() {
+
+	}
+
+}
