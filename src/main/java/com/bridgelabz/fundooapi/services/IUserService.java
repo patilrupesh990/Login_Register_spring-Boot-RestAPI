2cc68913fb10bccf6432c8069f568ead162cafc9
Rupesh <patilrupesh990@gmail.com>
2020-01-22 19:30:35 +0530

    [Rupesh-patil].upadated UserService interface

--- src/main/java/com/bridgelabz/fundooapi/services/IUserService.java
+++ src/main/java/com/bridgelabz/fundooapi/services/IUserService.java
@@ -5,37 +5,26 @@ import org.springframework.validation.BindingResult;
 
 import com.bridgelabz.fundooapi.model.User;
 import com.bridgelabz.fundooapi.model.UserLoginPair;
+import com.bridgelabz.fundooapi.response.UserData;
+import com.bridgelabz.fundooapi.response.UserResponse;
 
 
 public interface IUserService{
 
 	
 	
-	public User findUserByUserName(String userName);
 
-	public ResponseEntity<String>  registerUser(User user,BindingResult result);
+	public ResponseEntity<UserResponse> registerUser(User user,BindingResult result);
 
-	public long activateUserAccount(String token);
+	public ResponseEntity<String> activateUserAccount(String token);
 
 	public ResponseEntity<String> userLogin(UserLoginPair loginPair);   
 
 	public ResponseEntity<String> verifyBeforeResetPassword(String email);
 	
 	public ResponseEntity<String> resetPassword(String newPassword,String token);
-
-	
-//	public boolean loginUser(String email, String password);
-//	public boolean userExists(User user);
-//	public User getUserByEmail(String email, User user);
-//	public User getUserById(Integer id, User user);
-//	public void activateUser(Integer id);
-//	public void sendRegistrationVerificationLink(Integer id, String email)
-//			throws FileNotFoundException, ClassNotFoundException, IOException;
-//	public void sendLoginVerificationToken(User user, Token accessToken, HttpServletRequest request)
-//			throws FileNotFoundException, ClassNotFoundException, IOException;
-//	public void sendResetPasswordMail(User user, HttpServletRequest request, Token forgotToken)
-//			throws FileNotFoundException, ClassNotFoundException, IOException;
-//	public void resetPassword(User user);
+	public ResponseEntity<UserResponse> getUserList();
+	public ResponseEntity<UserData> getUserByID(String token);
 
 
 
