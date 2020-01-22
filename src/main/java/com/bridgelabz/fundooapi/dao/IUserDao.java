049c55e3b85f495976f7c683837dfc566e791bdc
Rupesh <patilrupesh990@gmail.com>
2020-01-22 19:29:07 +0530

    [Rupesh-patil].upadated UserDAo interface

--- src/main/java/com/bridgelabz/fundooapi/dao/IUserDao.java
+++ src/main/java/com/bridgelabz/fundooapi/dao/IUserDao.java
@@ -1,60 +1,30 @@
 package com.bridgelabz.fundooapi.dao;
 
 
+import java.util.List;
+
 import com.bridgelabz.fundooapi.model.User;
 
 public interface IUserDao 
 {
-	/**
-	 * @param user
-	 * @return registering user into DB.
-	 */
+	
 	public Integer registerUser(User user);
 
-	/**
-	 * @param email
-	 * @param password
-	 * @return logs in user by authenticating user. if authentication success,
-	 *         returns true else false
-	 */
+	
 	public boolean loginUser(String email, String password);
 
-	/**
-	 * @param id
-	 *            activates a registered user by setting isValid to true
-	 */
+	
 	public void activateUser(long id);
 
-	/**
-	 * @param email
-	 * @param user
-	 * @return
-	 * retrieves user object by email.
-	 * returns null if not found
-	 */
+	
 	public User getUserByEmail(String email);
 
-	/**
-	 * @param user
-	 * @return
-	 * checks if user exists already in DB
-	 */
 	public boolean userExists(User user);
 
-	/**
-	 * @param id
-	 * @param user
-	 * @return
-	 * retrieves user object by taking user id as input
-	 * return null if not user found
-	 */
 	public User getUserById(Long id);
 
-	/**
-	 * @param email
-	 * @param password
-	 * resets password for particular email
-	 */
+	
 	public int updateUserPaword(long id, String newPassword);
 	public boolean isUserVerified(String email);
+	public List<User> getAllUser();
 }
