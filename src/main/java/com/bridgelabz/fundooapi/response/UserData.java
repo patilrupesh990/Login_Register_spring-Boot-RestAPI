6c6ea8eb056ea8cd88891a1c19a1b52eaf073449
Rupesh <patilrupesh990@gmail.com>
2020-01-22 19:30:03 +0530

    [Rupesh-patil].upadated UserData responseclass

---
+++ src/main/java/com/bridgelabz/fundooapi/response/UserData.java
@@ -0,0 +1,29 @@
+package com.bridgelabz.fundooapi.response;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+@Component
+public class UserData {
+	private int id;
+	private String email;
+	private String firstname;
+	private String lastname;
+	private String gender;
+	private String phNo;
+	private String dateOfBirth;
+	private String activate;
+	private int status;
+	private String response;
+
+	public UserData(int status,String response)
+	{
+		this.status=status;
+		this.response=response;
+	}
+	public UserData() {}
+}
