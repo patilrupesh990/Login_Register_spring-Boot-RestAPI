cffa75d08a7a458273183cc6cf907ac0d0746626
Rupesh <patilrupesh990@gmail.com>
2020-02-17 19:21:55 +0530

    [Rupesh-patil]. Upadeted WebMvcConfig

--- src/main/java/com/bridgelabz/fundooapi/configration/WebMvcConfig.java
+++ src/main/java/com/bridgelabz/fundooapi/configration/WebMvcConfig.java
@@ -1,9 +1,10 @@
 package com.bridgelabz.fundooapi.configration;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+@Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
 	@Bean
